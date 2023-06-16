928e72047221b39fd7c4372f7be21fab2c79dfbd
jalleeell <akljalil026@gmail.com>
2023-06-15 19:41:49 -0700

    new commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a qoute using fprintf
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"");
+	fprintf(stderr, "- Dora Korpar, 2015-10-19\n");
+	return (1);
+}

c627955ca59be8e04d3ccc084f1a0e7f5e6e29ed
jalleeell <akljalil026@gmail.com>
2023-06-16 17:11:16 -0700

    new commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,21 +3,21 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints numbers from 0 to 9
- *             separated by commas and followed by a space
+ * Description: A C program that prints numbers from 0 to 9 separated by commas
+ *              followed by a space
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	char c = '0';
+	int o = 0;
 
-	while (c <= '9')
+	while (o <= 9)
 	{
-	putchar(c);
-	putchar(',');
-	putchar(' ');
-	c++;
+		putchar(o + 48);
+		putchar(',');
+		putchar(' ');
+		o++;
 	}
 
 	putchar('\n');

96fb91b19c78b0f4ea2231d5788405dda4c6e90e
jalleeell <akljalil026@gmail.com>
2023-06-16 19:59:05 -0700

    new commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible different combinations of two digits
+ *              separated by comma and space
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 <= 8; num1++)
+	{
+	for (num2 = num1 + 1; num2 <= 9; num2++)
+		{
+		putchar(num1 + 48);
+		putchar(num2 + 48);
+
+			if (num1 + num2 != 113)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
+

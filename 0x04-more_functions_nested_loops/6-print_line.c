5f719819a1e5cf3e741e71abe2441b3e22cac3d6
jalleeell <akljalil026@gmail.com>
2023-06-22 12:59:53 -0700

    new commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,14 +8,18 @@
 
 void print_line(int n)
 {
-	if  (n <= 0)
+	if (n <= 0)
+	{
 		_putchar('\n');
-	else
+	} else
 	{
 		int m;
 
 		for (m = 1; m <= n; m++)
+		{
 			_putchar('_');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+
 }

08fcd01652dd45b2c227f796efd81911cde15515
jalleeell <akljalil026@gmail.com>
2023-07-05 11:58:13 -0700

    new commit

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -5,12 +5,18 @@
  * @n: the given number
  *
  * Return: the factorial of n
-*/
+ */
 
 int factorial(int n)
 {
-	if (n <= 1)
+	if (n == 1)
+	{
 		return (1);
-
-	return (n * factorial(n - 1));
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+		return (n * factorial(n - 1));
 }

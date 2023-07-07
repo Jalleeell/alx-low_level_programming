202f2a3e8eef4db2a024f6bd2fd5e6b7efb894f9
jalleeell <akljalil026@gmail.com>
2023-07-07 11:22:11 -0700

    new commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - a program that prints its name, followed by a new line
+ * @argc: argument counter
+ * @argv: argument
+ *
+ * Return: 0
+ */
+
 int main(int argc, char **argv)
 {
 	(void)argc;

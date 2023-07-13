7e61a8b92c9972a8ded192f3c7e7902b49ac0059
jalleeell <akljalil026@gmail.com>
2023-07-13 16:00:40 -0700

    new commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: memory to allocate
+ *
+ * Return: poiner to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}

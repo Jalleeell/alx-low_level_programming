5132a5590b498d954bda2f3d74a578ca29dd65d1
jalleeell <akljalil026@gmail.com>
2023-07-13 17:48:53 -0700

    new commit

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,9 +4,6 @@
 
 #include <stdlib.h>
 
-char *_strncat(char *dest, char *src, int n);
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);

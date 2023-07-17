67a035748c025dd39135851d44319d6b1a069fd9
jalleeell <akljalil026@gmail.com>
2023-07-17 15:00:46 -0700

    new commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+/**
+ * struct dog - dog's info
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif /* DOG_H */

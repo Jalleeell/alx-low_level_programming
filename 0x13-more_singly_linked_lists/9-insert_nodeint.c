daf47327649512d234e910b1854ab0512584c0b9
jalleeell <akljalil026@gmail.com>
2023-08-02 14:49:44 -0700

    new commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,41 +4,41 @@
  * insert_nodeint_at_index - inserts a new node in a linked list,
  * at a given position
  * @head: pointer to the first node in the list
- * @idx: index where the nv node is added
- * @n: data to insert in the nv node
+ * @idx: index where the new node is added
+ * @n: data to insert in the new node
  *
- * Return: pointer to the nv node, or NULL
+ * Return: pointer to the new node, or NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *nv;
-	listint_t *nx = *head;
+	listint_t *new;
+	listint_t *temp = *head;
 
-	nv = malloc(sizeof(listint_t));
-	if (!nv || !head)
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
 		return (NULL);
 
-	nv->n = n;
-	nv->next = NULL;
+	new->n = n;
+	new->next = NULL;
 
 	if (idx == 0)
 	{
-		nv->next = *head;
-		*head = nv;
-		return (nv);
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
 
-	for (i = 0; nx && i < idx; i++)
+	for (i = 0; temp && i < idx; i++)
 	{
 		if (i == idx - 1)
 		{
-			nv->next = nx->next;
-			nx->next = nv;
-			return (nv);
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
 		}
 		else
-			nx = nx->next;
+			temp = temp->next;
 	}
 
 	return (NULL);

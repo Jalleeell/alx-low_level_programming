d11854887551439a01f20a63195b1a26dd90f465
jalleeell <akljalil026@gmail.com>
2023-08-03 19:09:29 -0700

    new commit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,13 +10,13 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int h;
+	unsigned int m;
 
 	if (index > 63)
 		return (-1);
 
 	m = 1 << index;
-	*n = (*n | h);
+	*n = (*n | m);
 
 	return (1);
 }
